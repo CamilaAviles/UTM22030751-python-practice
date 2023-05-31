67483c762e5602630f7796105e99acf3f0c8c6a0
Camila Aviles <camilanashv2003@gmail.com>
2023-05-31 13:54:05 -0600

    feat: Function

--- exercises.py
+++ exercises.py
@@ -1,10 +1,11 @@
 # List
 print("LIST")
 print ("These are the shoe brands we manage: ")
-shoes = ["Nike, Adidas, Converse, Puma,Under Armour"]
+shoes = ["Nike, Adidas, Converse, Puma, Under Armour"]
 all_shoes = shoes[0:4]
 print(all_shoes)
-newshoe =(input("What brand would you like to add?: "))
+
+newshoe =(input("Another brand you would like to know?: "))
 shoes.append(newshoe)
 for shoe in shoes:
     print(shoe)
@@ -22,9 +23,33 @@ while indice < len(family):
     indice += 1
 print("THIS IS ALL FOR TUPLE")
 
+
 # Dictionary
 print("DICTIONARY")
 personal_info = {'name': 'Camila', 'age':'19', 'city': 'Aguascalientes'} #declaring my dictionary
 personal_info['city'] = "Nashville" #modifying the city from were I am
 print(personal_info) #printing modified dictionary
 print("THIS IS ALL FOR DICTIONARY")
+
+
+# Function
+print ("FUNCTION")
+operand = input("What operation would you like to do? (+, -, /, *): ")
+firstNumber = float(input("Please enter your first number: "))
+secondNumber = float(input("Now enter your second number: "))
+
+def operation(operand, firstNumber, secondNumber):
+    if operand == '+':
+        result = firstNumber + secondNumber
+    elif operand == '-':
+        result = firstNumber - secondNumber
+    elif operand == '/':
+        result = firstNumber / secondNumber
+    elif operand == '*':
+        result = firstNumber * secondNumber
+    return result
+    
+result = operation (operand, firstNumber, secondNumber)
+print(result)    
+
+print("THIS IS ALL FOR FUNCTION")
